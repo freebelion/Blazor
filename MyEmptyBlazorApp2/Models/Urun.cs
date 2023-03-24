898ef23988d0a3922ed2e6c93630b647f3301628
freebelion <freebelion@gmail.com>
2023-03-24 16:01:58 +0300

    MyEmptyBlazorApp2 projesini (Bilgi girişine izin veren ürün listeleme uygulaması) ekledim.

---
+++ MyEmptyBlazorApp2/Models/Urun.cs
@@ -0,0 +1,17 @@
+﻿namespace MyEmptyBlazorApp2.Models
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public double Fiyat { get; set; }
+
+        // "Ad" özelliğinin türü string? değil, yani null değeri alamaz.
+        // yani boş kurucu fonksiyonda hiç değilse boş bir string almalı.
+        public Urun()
+        {
+            Ad = string.Empty;
+            Fiyat = 0;
+        }
+    }
+}
